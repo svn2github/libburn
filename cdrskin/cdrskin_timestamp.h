7912a1cf5768cfdad2ff56a12c779066377da570
scdbackup <scdbackup@fff8544e-141b-0410-a922-c1ea1f00b384>
2014-01-15 17:49:04 +0000

    Updated copyright claim in sg-linux.c
    git-svn-id: http://svn.libburnia-project.org/libburn/trunk@5225 fff8544e-141b-0410-a922-c1ea1f00b384

--- cdrskin/cdrskin_timestamp.h
+++ cdrskin/cdrskin_timestamp.h
@@ -1,1 +1,1 @@
-#define Cdrskin_timestamP "2014.01.09.214841"
+#define Cdrskin_timestamP "2014.01.15.174741"
--- libburn/sg-linux.c
+++ libburn/sg-linux.c
@@ -1,7 +1,7 @@
 /* -*- indent-tabs-mode: t; tab-width: 8; c-basic-offset: 8; -*- */
 
 /* Copyright (c) 2004 - 2006 Derek Foreman, Ben Jansens
-   Copyright (c) 2006 - 2013 Thomas Schmitt <scdbackup@gmx.net>
+   Copyright (c) 2006 - 2014 Thomas Schmitt <scdbackup@gmx.net>
    Provided under GPL version 2 or later.
 */
 
